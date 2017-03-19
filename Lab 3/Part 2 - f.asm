1331a42eeb38a0853824ce0b30f2922275084f33
AliSalemwala <aali_altaf@yahoo.com>
2017-03-19 13:56:29 +0300

    Updated ReadMe and made FastSort
    Made FastSort (uses quicksort algorithm) on the FIRST try no redo's.
    Just bragging

--- Lab 3/Part 2 - f.asm
+++ Lab 3/Part 2 - f.asm
@@ -3,7 +3,7 @@ main:
 .data
 	newLine:	.asciiz "\n"
 .text
-	li $a0, 2
+	li $a0, 3
 	jal fillArray
 	move $t0, $a0
 	move $a1, $v0
@@ -32,3 +32,54 @@ quit:
 	li $v0, 10
 	syscall
 FastSort:
+	ble $a1, 1, return
+	addi $sp, $sp, -32
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	sw $a1, 8($sp)
+	sw $s0, 12($sp)
+	sw $s1, 16($sp)
+	sw $s2, 20($sp)
+	sw $t0, 24($sp)
+	sw $t1, 28($sp)
+	sll $s0, $a1, 2
+	add $s0, $a0, $s0
+	lw $s1, ($a0)		# $s1 contains the pivot
+	addi $s2, $a0, 4	# $s2 contains beginning of less than pivot portion
+sortPartition:
+	addi $a0, $a0, 4
+	beq $a0, $s0, done
+	lw $t0, ($a0)
+	bgt $t0, $s1, gTPivot
+	j sortPartition
+gTPivot:
+	lw $t1, ($s2)
+	sw $t0, ($s2)
+	sw $t1, ($a0)
+	addi $s2, $s2, 4
+	j sortPartition
+done:
+	addi $s2, $s2, -4
+	lw $s0, ($s2)
+	lw $a0, 4($sp)
+	sw $s1, ($s2)
+	sw $s0, ($a0)
+	srl $a1, $a1, 1
+	jal FastSort
+	sll $t0, $a1, 2
+	add $a0, $a0, $t0
+	lw $a1, 8($sp)
+	srl $t0, $t0, 2
+	sub $a1, $a1, $t0
+	jal FastSort
+	lw $ra, 0($sp)
+	lw $a0, 4($sp)
+	lw $a1, 8($sp)
+	lw $s0, 12($sp)
+	lw $s1, 16($sp)
+	lw $s2, 20($sp)
+	lw $t0, 24($sp)
+	lw $t1, 28($sp)
+	addi $sp, $sp, 32
+return:
+	jr $ra
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CS-224-Labs
-The questions and solutions of labs for CS 224 (Spring Semester 2017) in Bilkent University
+The questions and solutions of labs for CS 224 (Spring Semester 2017) in Bilkent University.
+Done in MIPS Assembly Language
